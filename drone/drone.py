3c95bf1e30a824dc5a85a3edf0aea5bb1ef5c98d
BomboCato <briac.guellec@gmail.com>
2024-05-29 11:57:45 +0200

    feat(drone): creating a graph of all Montreal, and dividing this graph into a smaller one containing the 5 districts and the infos of the big one

--- drone/drone.py
+++ drone/drone.py
@@ -16,15 +16,25 @@ def retrieveGraph(file):
     return ox.load_graphml('drone/' + file)
 
 # saveMontrealGraph(filename)
-# G = retrieveGraph(filename)
+G = retrieveGraph(filename)
 
-G = ox.graph_from_place('Verdun, Montreal', network_type='drive')
+G1 = ox.graph_from_place('Outremont, Montreal', network_type='drive')
+G2 = ox.graph_from_place('Verdun, Montreal', network_type='drive')
+G3 = ox.graph_from_place('Plateau Mont-Royal, Montreal', network_type='drive')
+G4 = ox.graph_from_place('Rivière-des-Prairies–Pointe-aux-Trembles, Montreal', network_type='drive')
+G5 = ox.graph_from_place('Anjou, Montreal', network_type='drive')
+G6 = nx.compose_all([G1, G2, G3, G4, G5])
 
-print("G.edges: ", G.edges(keys=True, data=True))
-# print("G: ", G.edges(keys=True, data=True))
 for u, v in G.edges():
-    print("u: ", u)
-    print("v: ", v)
     G[u][v][0]['snow'] = random.randint(0, 15)
 ec = ['r' if (G[u][v][0]['snow'] >= 2.5 and G[u][v][0]['snow'] <= 15) else 'b' for u, v, k in G.edges(keys=True)]
-ox.plot_graph(G, edge_color=ec)
+
+# PARCOURS DRONE SUR G (AJOUTER UN ATTRIBUT POUR DIRE SI IL FAUT DENEIGER)
+
+
+# FAIRE UNE FONCTION QUI RETOURNE LE GRAPHE G6 AVEC CE NOUVEL ATTRIBUT (retourner R quoi)
+R=G.copy()
+R.remove_nodes_from(n for n in G if n not in G6)
+
+
+ox.plot_graph(R, edge_color=ec)
