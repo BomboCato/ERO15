93e9bba707c88176522b4ad28f084bf2838a9235
BomboCato <briac.guellec@gmail.com>
2024-05-29 10:46:12 +0200

    feat(drone): generating random amount of snow in each edge + coloring

--- drone/drone.py
+++ drone/drone.py
@@ -1,4 +1,7 @@
 import osmnx as ox
+import networkx as nx
+import random
+import scipy as sp
 
 filename = 'montreal.osm'
 
@@ -13,5 +16,15 @@ def retrieveGraph(file):
     return ox.load_graphml('drone/' + file)
 
 # saveMontrealGraph(filename)
-G = retrieveGraph(filename)
-ox.plot_graph(G)
+# G = retrieveGraph(filename)
+
+G = ox.graph_from_place('Verdun, Montreal', network_type='drive')
+
+print("G.edges: ", G.edges(keys=True, data=True))
+# print("G: ", G.edges(keys=True, data=True))
+for u, v in G.edges():
+    print("u: ", u)
+    print("v: ", v)
+    G[u][v][0]['snow'] = random.randint(0, 15)
+ec = ['r' if (G[u][v][0]['snow'] >= 2.5 and G[u][v][0]['snow'] <= 15) else 'b' for u, v, k in G.edges(keys=True)]
+ox.plot_graph(G, edge_color=ec)
