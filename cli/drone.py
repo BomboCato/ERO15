bad6409cba31d7d293dcaeea24643d8ee1582c25
Ender <ndxendernight@gmail.com>
2024-06-05 01:32:55 +0200

    Finished implementing video

--- cli/drone.py
+++ cli/drone.py
@@ -2,13 +2,13 @@
 # cli/drone.py
 #
 
-from typing import Annotated
+from typing import Annotated, Optional
 from rich.console import Console
-
-import typer
 from data.display import route_video
 from data.districts import District
 from data.route import Route
+
+import typer
 import drone.analyze
 import cli.log as log
 
@@ -27,16 +27,30 @@ def analyze(
     district: Annotated[
         str, typer.Argument(help="The district/city to analyze.")
     ] = "Montreal",
+    video: Annotated[
+        Optional[str],
+        typer.Option(
+            help="Generate a video that represents the drone route. Save it to [VIDEO].mp4"
+        ),
+    ] = None,
+    nb_threads: Annotated[
+        int,
+        typer.Option(
+            help="Number of threads to use when eulerizing or video generation"
+        ),
+    ] = 1,
 ) -> None:
     """
     Launch the drone and analyze the distict/city.
     """
 
-    log.info(f"CMD: analyze '{district}'")
+    log.info(f"CMD: drone analyze '{district}' threads: '{nb_threads}'")
 
     snow_eul, circuit = drone.analyze.analyze_snow(district)
 
     dist = District("snow_eul", snow_eul)
     route = Route(circuit, "snow_eul")
 
-    route_video(dist, route, "red", "save", 16)
+    if video:
+        log.info("Generating video")
+        route_video(dist, route, "red", video, nb_threads)
